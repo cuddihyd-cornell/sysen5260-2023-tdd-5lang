0c4106a403f1ca4cd8969eb1dc3ee11bf51c1961
Caitlin Jones <cbj42@cornell.edu>
2023-02-25 16:17:53 -0500

    updated matmpy.py
    Implemented mat_mpy function

--- py/matmpy.py
+++ py/matmpy.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def mat_mpy(mat_a: pd.DataFrame, mat_b: pd.DataFrame) -> pd.DataFrame:
-    """Return the mastrix-multiplication of mat_a and mat_b."""
-    // TODO: Implement Matrix Multiply
+    result = mat_a.dot(mat_b) # this method computes the matrix multiplication between the DataFrame and values of another series, DataFrame or a NumPy array.
+    return result
 
